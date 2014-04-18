f9f254c9f4897e398e0b9dbd384c03f76516dc39
melody-rain <xiah_sunny@hotmail.com>
2014-04-18 17:36:19 +0800

    added Sort List

---
+++ leetcode4.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+using namespace std;
+
+/**
+* Definition for singly-linked list.
+*/
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode *sortList(ListNode *head) {
+
+        int minimum;
+
+        ListNode *p1 = NULL;
+        ListNode *pre = NULL;
+
+        ListNode *temp = head->next;
+        ListNode *minNode = head->next;
+        ListNode *tempHead = head;
+
+        bool isMin = true;
+
+        while (temp)
+        {
+            minimum = temp->val;
+
+            while (temp->next)
+            {
+                if (minimum > temp->next->val)
+                {
+                    minimum = temp->next->val;
+                    minNode = temp->next;
+                    pre = temp;
+                }
+
+                temp = temp->next;
+            }
+
+            pre = minNode->next;
+
+            p1 = tempHead->next;
+            tempHead->next = minNode;
+            minNode->next = p1;
+
+            temp = minNode->next;
+            tempHead = tempHead->next;
+
+            if (isMin)
+            {
+                head->next = minNode;
+                isMin = false;
+            }
+        }
+
+        return head;
+
+    }
+};
+
+int main()
+{
+    ListNode in0(5);
+    ListNode in1(9);
+    ListNode in2(8);
+    ListNode in3(10);
+    ListNode in4(4);
+    ListNode in5(1);
+
+    in0.next = &(in1);
+    in1.next = &(in2);
+    in2.next = &(in3);
+    in3.next = &(in4);
+    in4.next = &(in5);
+
+    ListNode *head = new ListNode(0);
+    head->next = &(in0);
+
+    Solution mySolution;
+    ListNode* output = mySolution.sortList(head);
+    for (int i = 0; i < 5; i++)
+    {
+        cout << "%d" << (output++)->val;
+    }
+}
