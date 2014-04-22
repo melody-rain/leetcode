b5ea5ff93dceb12330507e7119247459bdf34839
melody_rain <xiah_sunny@hotmail.com>
2014-04-22 21:50:28 +0800

    use STL's hash map instead of creating the hap table by myself.

--- leetcode7.cpp
+++ leetcode7.cpp
@@ -1,99 +1,31 @@
 #include <iostream>
 #include <vector>
+#include <map>
 
 using namespace std;
 
-class Solution
-{
+class Solution {
 public:
-    vector<int> twoSum(vector<int> &numbers, int target)
-    {
-        int index1 = 0, index2 = 0;
-        HashTable myHashTable(numbers.size());
-        myHashTable.createHashTable(numbers);
-
-        int rest = 0;
-        for (unsigned int i = 0; i < numbers.size(); i++)
-        {
-            index1 = i;
-            rest = target - numbers[index1];
-            if (rest < 0)
-                continue;
-            if (myHashTable.searchKey(rest, numbers.size(), index1, index2))
-            {
-                break;
+    vector<int> twoSum(vector<int> &numbers, int target) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        map<int, int> table;
+        map<int, int>::iterator iter;
+        vector<int> result;
+        int comp;
+        for (unsigned int i = 0; i < numbers.size(); ++i) {
+            comp = target - numbers[i];
+            iter = table.find(comp);
+            if (iter == table.end()) {
+                table.insert(pair<int, int>(numbers[i], i));
+            }
+            else {
+                result.push_back(iter->second + 1);
+                result.push_back(i + 1);
+                return result;
             }
         }
-
-        vector<int>index;
-        index.push_back(index1 + 1);
-        index.push_back(index2 + 1);
-        return index;
+        return result;
     }
-
-private:
-    struct HashTable
-    {
-        int *elem;
-        int count;
-        int *indexRecoder;
-
-        HashTable(int size)
-        {
-            count = size;
-            elem = new int[count];
-            indexRecoder = new int[count];
-            for (int i = 0; i < count; i++)
-            {
-                elem[i] = -32768;
-            }
-        }
-
-        ~HashTable()
-        {
-            delete[] elem;
-        }
-
-        void createHashTable(vector<int> &numbers)
-        {
-            for (unsigned int i = 0; i < numbers.size(); i++)
-            {
-                int addr = numbers[i] % numbers.size();
-
-                while (elem[addr] != -32768)
-                {
-                    addr = (addr + 1) % numbers.size();
-                }
-
-                elem[addr] = numbers[i];
-                indexRecoder[addr] = i;
-            }
-        }
-
-        bool searchKey(int key, int width, int index1, int& index2)
-        {
-            int addr = key % width;
-
-            while (1)
-            {
-                while (elem[addr] != key)
-                {
-                    addr = (addr + 1) % width;
-                    if (elem[addr] == -32768 || addr == (key % width))
-                        return false;
-                }
-
-                index2 = indexRecoder[addr];
-
-                if (index1 < index2)
-                    return true;
-                else
-                {
-                    addr = (addr + 1) % width;
-                }
-            }
-        }
-    };
 };
 
 int main()
@@ -109,8 +41,8 @@ int main()
         input.push_back(1);
         input.push_back(9);
         input.push_back(4);
-        input.push_back(4);
         input.push_back(56);
+        input.push_back(4);
         input.push_back(90);
         input.push_back(3);
     }
